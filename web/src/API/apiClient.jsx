338183647b7f3f9fe865144c0092be1d4bb9cc4f
Estefany Torres <estefanyt3101@gmail.com>
2024-12-16 16:42:06 -0500

    Nuevo

--- web/src/API/apiClient.jsx
+++ web/src/API/apiClient.jsx
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000"
-})
-
-export default apiClient;
+    baseURL: "http://fundoscorpius.com:3000"
+  })
+  
+  export default apiClient;
